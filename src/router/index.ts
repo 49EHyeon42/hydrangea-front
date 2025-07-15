279f627757a42ca2f3885f5da8e0a17a88b132ee
49EHyeon42 <49ehyeon42@gmail.com>
2025-07-15 21:09:35 +0900

    fix: 메인 이동 시 로그인 화면으로 이동할 수 있도록 수정

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,10 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      redirect: '/sign-in',
+    },
+    {
       path: '/sign-in',
       name: 'SignInView',
       component: () => import('@/views/SignInView.vue'),
@@ -25,11 +29,15 @@ const router = createRouter({
 router.beforeEach((to, from, next) => {
   const authStore = useAuthStore();
 
-  if (!authStore.isSignedIn) {
-    next({ name: 'SignInView' });
-  } else {
-    next();
+  if (!authStore.isSignedIn && !(to.name === 'SignInView')) {
+    return next({ name: 'SignInView' });
   }
+
+  if (authStore.isSignedIn && to.name === 'SignInView') {
+    return next({ name: 'ChatView' });
+  }
+
+  next();
 });
 
 export default router;
